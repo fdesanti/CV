1c8d15ae273fd02aa179b0648039387e53094d67
Federico De Santi <f.desanti@campus.unimib.it>
2026-09-20 02:45:47 +0200

    added flags to toggle papers and talks

--- database.py
+++ database.py
@@ -26,21 +26,13 @@ if submitted:
     papers['submitted']['label'] = 'Submitted papers'
     papers['submitted']['data'] = []
 
-    papers['submitted']['data'].append({
-#         author  = {De Santi, Federico and Mould, Matthew and Gerosa, Davide},
-#   title   = {Turning the likelihood around: a population-sampling approach to
-#              gravitational-wave hierarchical inference},
-#   year    = {2026},
-#   note    = {in preparation},
-        "title":    "Turning the likelihood around: a population-sampling approach to gravitational-wave hierarchical inference",
-        "author":   "F. De Santi, M. Mould, D. Gerosa",
-        "journal":  "(2026)",
-        "link":     "",
-        "arxiv":    "",
-        "ads":      "",
-        "inspire":  "",
-        "more":     "In preparation"
-        })
+    # papers['submitted']['data'].append({
+    #   author  = {De Santi, Federico and Mould, Matthew and Gerosa, Davide},
+    #   title   = {Turning the likelihood around: a population-sampling approach to
+    #              gravitational-wave hierarchical inference},
+    #   year    = {2026},
+    #   note    = {in preparation},
+
 
  
     # papers['submitted']['data'].append({
